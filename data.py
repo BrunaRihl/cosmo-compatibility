53f946f1aaf2ff76876f95688b3d2cfaa348150c
BrunaRihl <brunarihl@gmail.com>
2023-10-19 19:07:05 +0100

    feat: add dictionary with signs and their corresponding date ranges

--- data.py
+++ data.py
@@ -1,2 +1,52 @@
 from datetime import date
 
+signs = {
+    "Aries": {
+        "start": date(2023, 3, 21),
+        "end": date(2023, 4, 20)
+    },
+    "Taurus": {
+        "start": date(2023, 4, 21),
+        "end": date(2023, 5, 20)
+    },
+    "Gemini": {
+        "start": date(2023, 5, 21),
+        "end": date(2023, 6, 22)
+    },
+    "Cancer": {
+        "start": date(2023, 6, 23),
+        "end": date(2023, 7, 22)
+    },
+    "Leo": {
+        "start": date(2023, 7, 23),
+        "end": date(2023, 8, 22)
+    },
+    "Virgo": {
+        "start": date(2023, 8, 23),
+        "end": date(2023, 9, 22)
+    },
+    "Libra": {
+        "start": date(2023, 9, 23),
+        "end": date(2023, 10, 22)
+    },
+    "Scorpio": {
+        "start": date(2023, 10, 23),
+        "end": date(2023, 11, 21)
+    },
+    "Sagittarius": {
+        "start": date(2023, 11, 22),
+        "end": date(2023, 12, 21)
+    },
+    "Capricorn": {
+        "start": date(2023, 12, 22),
+        "end": date(2023, 1, 19)
+    },
+    "Aquarius": {
+        "start": date(2023, 1, 20),
+        "end": date(2023, 2, 18)
+    },
+    "Pisces": {
+        "start": date(2023, 2, 19),
+        "end": date(2023, 3, 20)
+    },
+}
