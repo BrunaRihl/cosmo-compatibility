0789f6bea192e3c99d17b2cd09b73224e31c8faf
BrunaRihl <brunarihl@gmail.com>
2023-10-19 20:22:41 +0100

    feat: add functions to determine a person's zodiac sign

--- run.py
+++ run.py
@@ -1,3 +1,17 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+from data import data, signs
+import datetime
+
+def get_current_year():
+    today = datetime.date.today()
+    return  today.year
+
+
+def get_sign(day, month):
+    year = get_current_year()
+    birthday = datetime.date(year, month, day)
+    
+    for sign in signs:
+        period = signs[sign]
+        if period["start"] <= birthday <= period["end"]:
+            return sign
+    
