29932ff576019b4fc29e9192482fd61bf479f109
BrunaRihl <brunarihl@gmail.com>
2023-10-20 16:17:48 +0100

    feat: implement loops in functions to initiate the test process

--- run.py
+++ run.py
@@ -1,5 +1,6 @@
-from data import data, signs
+from data import signs
 import datetime
+import pyfiglet
 
 def get_current_year():
     today = datetime.date.today()
@@ -16,21 +17,32 @@ def get_sign(day, month):
             return sign
     return "Invalid month or day"
 
+
 def initial_screen():
     font = pyfiglet.Figlet()
     message = "Cosmo * Compatibility"
     rendered_message = font.renderText(message)
     print(rendered_message)
     print("Welcome to the CosmoCompatibility Zodiac Traits Test!\nThis program will calculate the percentage of traits you share in common with your zodiac sign.")
+    while True:
+        play_test = str(input('Do you want to start the test? (y/n): '))
+        if play_test in 'yYnN':
+            return play_test
+
 
 def main():
-    day = int(input("Enter the day of your birth: "))
-    month = int(input("Enter the month of your birth: "))
+    play = initial_screen()
+    if play in 'Yy':
+        while True:
+            day = int(input("Enter the day of your birth (1-31): "))
+            month = int(input("Enter the month of your birth (1-12): "))
 
-    sign = get_sign(day, month)
-    sign = sign.lower()
-    print(f"Your zodiac sign is {sign}")
+            sign = get_sign(day, month)
+            sign = sign.lower()
+            print(f"Your zodiac sign is {sign}")
+        break
+
 
 main()
 
-    
+
