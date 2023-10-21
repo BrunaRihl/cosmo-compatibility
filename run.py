b2553373dadb09d449e04a7272c4d1ac2b90caf2
BrunaRihl <brunarihl@gmail.com>
2023-10-21 17:42:47 +0100

    refactor: updatd functions for better applicability in the code

--- run.py
+++ run.py
@@ -1,33 +1,35 @@
-from data import signs
+from data import data, signs
 import datetime
 import pyfiglet
 
-def get_current_year():
-    today = datetime.date.today()
-    return  today.year
+
+def get_year():
+    # solution to use datetime independent of the current year
+    return 2023
 
 
 def get_sign(day, month):
-    year = get_current_year()
+    year = get_year()
     birthday = datetime.date(year, month, day)
     
     for sign in signs:
         period = signs[sign]
         if period["start"] <= birthday <= period["end"]:
             return sign
+    
     return "Invalid month or day"
 
-
-def initial_screen():
+def header():
     font = pyfiglet.Figlet()
     message = "Cosmo * Compatibility"
     rendered_message = font.renderText(message)
     print(rendered_message)
+
+
+def initial_screen():
+    clear()
+    header()
     print("Welcome to the CosmoCompatibility Zodiac Traits Test!\nThis program will calculate the percentage of traits you share in common with your zodiac sign.")
-    while True:
-        play_test = str(input('Do you want to start the test? (y/n): '))
-        if play_test in 'yYnN':
-            return play_test
 
 
 def main():
